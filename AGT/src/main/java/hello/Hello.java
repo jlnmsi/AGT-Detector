8992448d813f508bde10b19489b85708e7fdfb4a
jlnmsi <jlnmsi@gmail.com>
2017-04-19 13:40:37 +0200

    first

---
+++ AGT/src/main/java/hello/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package hello;
+
+/**
+ * @author jlnmsi
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
